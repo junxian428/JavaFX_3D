5d17ef6489869e105c8ccafd547d38667054980a
junxian428 <holeyiho@gmail.com>
2023-04-17 11:59:08 +0800

    Height Width Length adjust for 3D object

--- src/CADController.java
+++ src/CADController.java
@@ -2,11 +2,16 @@ import java.util.ResourceBundle;
 
 import javax.print.DocFlavor.URL;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.SubScene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
@@ -19,7 +24,10 @@ public class CADController  implements Initializable {
     @FXML private PerspectiveCamera camera;
 
     @FXML private Box myBox;
+
+    @FXML private Label Label3DProperties;
     
+    @FXML private TextField lengthparameterinput,heightparameterinput,depthparameterinput;
     //public void initialize(URL url, ResourceBundle rb) {
         
     //}
@@ -33,6 +41,9 @@ public class CADController  implements Initializable {
         material.setDiffuseColor(Color.RED);
         myBox.setMaterial(material);
         my3DGroup.getChildren().add(myBox);
+        //EventHandler<MouseEvent> mouseClickHandler = (EventHandler<MouseEvent>) myBox.getOnMouseClicked();
+        //System.out.println("Current mouse click handler: " + mouseClickHandler);
+
         
         //PerspectiveCamera camera = new PerspectiveCamera(true);
         camera.setNearClip(0.1);
@@ -43,4 +54,39 @@ public class CADController  implements Initializable {
         //my3DGroup.getScene().setCamera(camera);
         //throw new UnsupportedOperationException("Unimplemented method 'initialize'");
     }
+
+    @FXML
+    public void ShapeClick(){
+        //System.out.println("It is clicked ");
+        //System.out.println(myBox.getWidth());
+        //System.out.println(myBox.getHeight());
+        //System.out.println(myBox.getDepth());
+        lengthparameterinput.setText(Double.toString(myBox.getWidth()));
+        heightparameterinput.setText(Double.toString(myBox.getHeight()));
+        depthparameterinput.setText(Double.toString(myBox.getDepth()));
+
+    }
+
+    @FXML
+    public void AdjustLength(ActionEvent event){
+        myBox.setWidth(Double.parseDouble(lengthparameterinput.getText()));
+        System.out.println(lengthparameterinput.getText());
+        
+    }
+
+    @FXML
+    public void AdjustHeight(ActionEvent event){
+        myBox.setHeight(Double.parseDouble(heightparameterinput.getText()));
+        System.out.println(heightparameterinput.getText());
+        
+    }
+
+    @FXML
+    public void AdjustDepth(ActionEvent event){
+        myBox.setDepth(Double.parseDouble(depthparameterinput.getText()));
+        System.out.println(depthparameterinput.getText());
+        
+    }
+
+  
 }
